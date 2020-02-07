afc8fd6243c7a8d0da5d1ff6fbb644ddde13e6a7
S00189591 <S00189591@LAB-D2036-07>
2020-02-07 10:46:17 +0000

    Q5,Q6,Q7,Q8

---
+++ Q5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            /*Chanied Linq queries*/
+
+            var query1 = numbers
+                .OrderByDescending(n => n);
+            var query2 = query1
+         .Where(n => n < 8);
+            var query3 = query2
+         .Select(n => DoubleIt(n));
+
+            /*Chained Landa Quieries*/
+
+            //var query1 = from number in numbers
+            //             orderby number descending
+            //             select number;
+
+            //var query2 = from number in numbers
+            //             orderby number < 8
+            //             select number;
+
+            //var query3 = from number in numbers
+            //             orderby number descending
+            //             select DoubleIt(number);
+
+            var query4 = numbers
+                .OrderByDescending(n => n)
+                .Where(n => n < 8)
+                .Select(n => DoubleIt(n));
+                
+
+            foreach (var item in query3)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static int DoubleIt(int value)
+        {
+            Console.WriteLine("About to double the number " + value.ToString());
+            return value * 2;
+        }
+
+
+    }
+}
---
+++ Q6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            /*Linq Query*/
+            //var displayNames = from name in names
+            //                   select name;
+
+            /*Q7 Order above alpabetically
+     
+            //var displayNames = from name in names
+            //                   orderby name
+            //                   select name;
+
+            /*Q8 Include only names that begin with M*/
+
+            var displayNames = from name in names
+                               where name.StartsWith("M")
+                               orderby name
+                               select name;
+
+            ///*Lamda Query*/
+            //var displayNames = names
+            //    .Select(n => n);
+
+            /*Q7 Order above alpabetically*/
+
+            //var displayNames = names
+            //    .OrderBy(n => n)
+            //    .Select(n => n);
+
+            ///*Q8 Include only names that begin with M*/
+            //var displayNames = names
+            //    .Where(n => n.StartsWith("M"))
+            //    .OrderBy(n => n)
+            //    .Select(n => n);
+
+            foreach (var name in displayNames)
+            {
+                Console.WriteLine(name);                       
+            }
+
+        }
+    }
+}
