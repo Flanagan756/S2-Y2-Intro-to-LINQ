9d1a754b883472c6eab4a266507ce1e09624b1e9
S00189591 <S00189591@LAB-D2036-07>
2020-02-07 11:01:46 +0000

    Q9_Q10

--- Q6/Program.cs
+++ Q6/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Q6
+namespace Q6_Q7_Q8
 {
 
     class Program
---
+++ Q9_Q10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q9_Q10
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+        }
+        
+
+    }
+    public class Customer
+    {
+        public string Name;
+        public string City;
+    }
+}
